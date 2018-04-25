930c1a5ddaeaed08e394ba795f70946850adc11a
Annie Alig <user@users-mac-mini.local>
2018-04-26 00:22:58 +0300

    should add constrains

--- MyCalculator/ViewController.swift
+++ MyCalculator/ViewController.swift
@@ -10,16 +10,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Properties
+    
+    @IBOutlet weak var totalTextField: UITextField!
+    @IBOutlet weak var taxPctSlider: UISlider!
+    @IBOutlet weak var taxPctLabel: UILabel!
+    @IBOutlet weak var resultsTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        refreshUI()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    //MARK: Actions
+    
+    @IBAction func calculateTapped(_ sender: Any) {
+        tipsCalc.total = Double((totalTextField.text! as NSString).doubleValue)
+        let possibleTips = tipsCalc.returnPossibleTips()
+        var results = ""
+        
+        for (tipPct, tipValue) in possibleTips {
+            results += "\(tipPct)%: \(tipValue)\n"
+        }
+        
+        resultsTextView.text = results
+    }
+    @IBAction func taxPercentageChanged(_ sender: Any) {
+        tipsCalc.taxPct = Double(taxPctSlider.value) / 100.0
+        refreshUI()
+    }
+    @IBAction func viewTapped(_ sender: Any) {
+        totalTextField.resignFirstResponder()
+    }
+    
+    //MARK: Methods
+    let tipsCalc = TipCalculatorModel(total:33.25, taxPct: 0.06)
+    
+    func refreshUI() {
+        totalTextField.text = String(format: "%0.2f", tipsCalc.total)
+        taxPctSlider.value = Float(tipsCalc.taxPct) * 100.0
+        taxPctLabel.text = "Tax Percentage (\(Int(taxPctSlider.value))%)"
+        resultsTextView.text = ""
+    }
 }
 
